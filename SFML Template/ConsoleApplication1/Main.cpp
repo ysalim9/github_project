c02474ccf09683e8a84252fb2e222e943c7f1c4d
bh8735424-cell <bh8735424@gmail.com>
2026-04-18 03:15:54 +0200

    نظفت جزء من ال main

--- SFML Template/ConsoleApplication1/Main.cpp
+++ SFML Template/ConsoleApplication1/Main.cpp
@@ -9,50 +9,23 @@
 #include <fstream>
 #include <SFML/Audio.hpp>
 #include <iomanip>
-
 using namespace std;
 using namespace sf;
 Character  player;
 Bullet bullet; 
-//----------------------------------shapes and variables-----------------------//
-//int colum = 0;
+//---------------------------------------------------------background---------------------------------------------------------//
 View view(sf::FloatRect(0, 0, 1000, 800));
 sf::RenderWindow window(sf::VideoMode(1080, 1050), "Zombies Project - Clean Version");
-
-//RectangleShape copyplayer(Vector2f(124 / 4, 128 / 2));
-//RectangleShape re1(Vector2f(100, 100)), re2(Vector2f(100, 100));
-//RectangleShape gun(Vector2f(25, 10));
-//CircleShape circle(10.f);
 Sprite background;
 Texture texturebacground;
 float dt;
-//Sprite player;
-//Vector2f aimdirection;
-//Texture mario;
 
-//----------------------------------------------guns variables------------------------------------------------------//
-//enum weopan { pistlo, rifle };
-//weopan currentweopon = pistlo;
-//#define pistlofirerate 1.0f
-//#define pistlodamage 25
-//#define riflofirerate 0.5f
-//#define riflodamage 15 
-//
-//float shoottimer = 0;
-//float currentfirerate = pistlofirerate;
-//int currentdamage = pistlodamage;
-//
-//int pistolAmmoLoaded = 8;
-//int rifleAmmoLoaded = 30;
-//float playersdpeed = 300.f;
-//int currentammo = 8;
-//bool isZPressed = false;
 
-//----------------------------------functions prototypes-----------------------------//
+//----------------------------------functions-----------------------------//
 void Start();
 void Draw();
 void Update(float dt);
-//----------------------------------bullet structure----------------------//
+
 
 
 
@@ -138,7 +111,6 @@ void Update(float dt);
 //    }
 //};
 //vector<zombie> zombielist;
-//vector<zombie> zombielist2;
 
 int main() {
     srand(static_cast<unsigned>(time(0)));
@@ -158,22 +130,9 @@ int main() {
 }
 
 void Start() {
-    //zombiespreite.loadFromFile("zombie.png");
-    ////zombiespreite2.loadFromFile("zombie2.png");
-    //mario.loadFromFile("mario.png");
-    //player.setTexture(mario);
-    //player.setTextureRect(IntRect(44, 2 * 62, 44, 62));
-    //player.setPosition(205, 205);
-    //player.setScale(1, 1);
-    //player.setOrigin(player.getLocalBounds().width / 2, player.getLocalBounds().height / 2);
-    //gun.setOrigin((gun.getLocalBounds().width / 2) - 100, gun.getLocalBounds().height / 2);
-    //circle.setOrigin(circle.getLocalBounds().width / 2, circle.getLocalBounds().height / 2);
-    //re1.setPosition(1000, 250);
-    //re2.setPosition(250, 250);
-
     window.setMouseCursorVisible(false);
-    //copyplayer.setOrigin(copyplayer.getLocalBounds().width / 2, copyplayer.getLocalBounds().height / 2);
     player.Startplayer();
+
     if (texturebacground.loadFromFile("background2.png")) {
         texturebacground.setRepeated(true);
         background.setTexture(texturebacground);
@@ -183,60 +142,18 @@ void Start() {
 }
 
 void Update(float dt) {
+    //-------------------------calling function of player------------------------------------//
     player.Updateplayer(dt, window);
     bullet.bullethandling(dt);
+    //----------------------------bakgriund-------------------------------------------------//
     view.setCenter(player.players.getPosition());
     background.setPosition(player.players.getPosition());
     int mapx = static_cast<int>(player.players.getPosition().x);
     int mapy = static_cast<int>(player.players.getPosition().y);
     background.setTextureRect(IntRect(mapx, mapy, 1000, 800));
-
-    //playermovement();
-    //calculuting();
-    //shooting();
-    //bullethandling();
-    //zombiehandling();
-    //playercollieer();
-    //switchwepoans();
-    //reload();
-    //addzombie();
-    //playeranimation();
 }
 
-//void playermovement() {
-//    up = false; down = false; a = false; d = false;
-//    if (Keyboard::isKeyPressed(Keyboard::W)) { player.move(0, -playersdpeed * deltaTime); up = true; }
-//    if (Keyboard::isKeyPressed(Keyboard::S)) { player.move(0, playersdpeed * deltaTime); down = true; }
-//    if (Keyboard::isKeyPressed(Keyboard::D)) { player.move(playersdpeed * deltaTime, 0); d = true; }
-//    if (Keyboard::isKeyPressed(Keyboard::A)) { player.move(-playersdpeed * deltaTime, 0); a = true; }
-//}
 
-    //void playeranimation() {
-    //    if (up) { colum = (colum + 1) % 12; player.setTextureRect(IntRect(colum * 44, 3 * 62, 44, 62)); }
-    //    else if (down) { colum = (colum + 1) % 12; player.setTextureRect(IntRect(colum * 44, 0 * 62, 44, 62)); }
-    //    else if (d) { colum = (colum + 1) % 12; player.setTextureRect(IntRect(colum * 44, 2 * 62, 44, 62)); }
-    //    else if (a) { colum = (colum + 1) % 12; player.setTextureRect(IntRect((colum + 1) * 44, 1 * 62, -44, 62)); }
-    //}
-
-//void playercollieer() {
-//
-//    if (copyplayer.getGlobalBounds().intersects(re2.getGlobalBounds())) {
-//        if (Keyboard::isKeyPressed(Keyboard::W)) player.move(0, playersdpeed * deltaTime);
-//        if (Keyboard::isKeyPressed(Keyboard::S)) player.move(0, -playersdpeed * deltaTime);
-//        if (Keyboard::isKeyPressed(Keyboard::D)) player.move(-playersdpeed * deltaTime, 0);
-//        if (Keyboard::isKeyPressed(Keyboard::A)) player.move(playersdpeed * deltaTime, 0);
-//    }
-//
-//
-//    for (int i = 0; i < bullets.size(); i++) {
-//        bool bulletRemoved = false;
-//
-//
-//        if (bullets[i].shape.getGlobalBounds().intersects(re2.getGlobalBounds())) {
-//            bullets.erase(bullets.begin() + i);
-//            i--;
-//            continue;
-//        }
 //
 //        for (int j = 0; j < zombielist.size(); j++) {
 //            if (!zombielist[j].isdead && bullets[i].shape.getGlobalBounds().intersects(zombielist[j].shape.getGlobalBounds())) {
@@ -250,39 +167,9 @@ void Update(float dt) {
 //    }
 //}
 
-//void calculuting() {
-//    Vector2i mouseposscreen = Mouse::getPosition(window);
-//    Vector2f mouseworldposition = window.mapPixelToCoords(mouseposscreen);
-//    circle.setPosition(mouseworldposition);
-//    aimdirection = mouseworldposition - player.getPosition();
-//    gun.setRotation(atan2(aimdirection.y, aimdirection.x) * 180 / 3.14159f);
-//    aimdirection = normalize(aimdirection);
-//    gun.setPosition(player.getPosition());
-//    copyplayer.setPosition(player.getPosition());
-//}
 
-//void shooting() {
-//    shoottimer += deltaTime;
-//    if (Mouse::isButtonPressed(Mouse::Left) && shoottimer >= currentfirerate && currentammo > 0) {
-//        shoottimer = 0;
-//        currentammo--;
-//        if (currentweopon == pistlo) pistolAmmoLoaded = currentammo;
-//        else rifleAmmoLoaded = currentammo;
-//
-//        Bullet newbullet;
-//        newbullet.shape.setSize(Vector2f(10, 5));
-//        newbullet.shape.setFillColor(Color::Cyan);
-//        newbullet.shape.setPosition(gun.getPosition());
-//        newbullet.shape.setRotation(gun.getRotation());
-//        newbullet.damage = currentdamage;
-//        newbullet.velocity = aimdirection * 800.0f;
-//        bullets.push_back(newbullet);
-//    }
-//}
-//
-//void bullethandling() {
-//    for (int i = 0; i < bullets.size(); i++) bullets[i].Update(deltaTime);
-//}
+
+
 
 //void zombiehandling() {
 //    float personalSpace = 50.0f;
@@ -341,22 +228,6 @@ void Update(float dt) {
 //    else isZPressed = false;
 //}
 
-//void switchwepoans() {
-//    if (Keyboard::isKeyPressed(Keyboard::Num1)) {
-//        currentweopon = pistlo; currentfirerate = pistlofirerate; currentdamage = pistlodamage; currentammo = pistolAmmoLoaded;
-//    }
-//    if (Keyboard::isKeyPressed(Keyboard::Num2)) {
-//        currentweopon = rifle; currentfirerate = riflofirerate; currentdamage = riflodamage; currentammo = rifleAmmoLoaded;
-//    }
-//}
-//
-//void reload() {
-//    if (Keyboard::isKeyPressed(Keyboard::R)) {
-//        if (currentweopon == pistlo) { pistolAmmoLoaded = 8; currentammo = 8; }
-//        else { rifleAmmoLoaded = 30; currentammo = 30; }
-//        cout << "reloading..." << endl;
-//    }
-//}
 
 void Draw() {
     window.clear();
@@ -365,10 +236,6 @@ void Draw() {
     player.Drawplayer(window);
     //window.draw(re1);
     //window.draw(re2);
-    //for (auto& b : bullets) window.draw(b.shape);
-    //window.draw(player);
-    //window.draw(circle);
-    //window.draw(gun);
     //for (auto& z : zombielist) window.draw(z.shape);
     window.display();
 }
